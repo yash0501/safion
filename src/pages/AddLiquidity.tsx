efc9c629c097ce6d6c9035f049dc62b5629a00d3
Yash Garg <yashgarg@goodeducator.com>
2024-11-23 23:54:41 +0530

    updated AddLiquidity and view LiquidityPositions

--- src/pages/AddLiquidity.tsx
+++ src/pages/AddLiquidity.tsx
@@ -1,23 +1,102 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "@/pages/Navbar.tsx";
 import StarfieldAnimation from "react-starfield";
+import { useTonAddress, TonConnectButton } from "@tonconnect/ui-react";
 
 const AddLiquidity = () => {
+  const tonAddress = useTonAddress(); // Fetch wallet address
   const [selectedPair, setSelectedPair] = useState({
-    token1: "ETH",
-    token2: "USDC",
+    token1: "TON",
+    token2: "USDT",
   });
   const [feeTier, setFeeTier] = useState(null);
   const [lowPrice, setLowPrice] = useState("");
   const [highPrice, setHighPrice] = useState("");
   const [depositAmount1, setDepositAmount1] = useState("");
   const [depositAmount2, setDepositAmount2] = useState("");
-  const [userAddress, setUserAddress] = useState("0xABC123..."); // Placeholder for user wallet address
+  const [userAddress, setUserAddress] = useState("");
+  const [isFormValid, setIsFormValid] = useState(false);
+  const [isWalletConnected, setIsWalletConnected] = useState(false);
 
-  const tokens = ["ETH", "USDC", "TON", "1INCH", "ZRX"];
+  useEffect(() => {
+    // Update userAddress and wallet connection status when wallet is connected
+    setUserAddress(tonAddress || ""); // Set empty string if tonAddress is null/undefined
+    setIsWalletConnected(!!tonAddress);
+  }, [tonAddress]);
 
-  // Function to handle the API call
+  // Validate form inputs
+  useEffect(() => {
+    const isValid =
+      isWalletConnected &&
+      selectedPair.token1 &&
+      selectedPair.token2 &&
+      feeTier !== null &&
+      lowPrice &&
+      highPrice &&
+      depositAmount1 &&
+      depositAmount2;
+
+    setIsFormValid(isValid);
+  }, [
+    isWalletConnected,
+    selectedPair,
+    feeTier,
+    lowPrice,
+    highPrice,
+    depositAmount1,
+    depositAmount2,
+  ]);
+
+  const tokens = ["ETH", "USDT", "TON", "1INCH", "ZRX"];
+
+  const updatePriceRange = async (amount, baseToken, targetToken, updateOtherAmount) => {
+    try {
+      const response = await fetch(
+        `https://safion-simple-backend.onrender.com/liquidity-providers/price/${baseToken}/${targetToken}?amount=${amount}`,
+        {
+          method: "GET",
+        }
+      );
+      if (response.ok) {
+        const data = await response.json();
+        const pricePerUnit = data.pricePerUnit;
+        const totalPrice = data.totalPrice;
+
+        setLowPrice((pricePerUnit / 2).toFixed(4));
+        setHighPrice((pricePerUnit * 2).toFixed(4));
+
+        // Update the other deposit amount automatically
+        if (updateOtherAmount) {
+          if (baseToken === selectedPair.token1) {
+            setDepositAmount2(totalPrice.toFixed(4));
+          } else {
+            setDepositAmount1(totalPrice.toFixed(4));
+          }
+        }
+      } else {
+        console.error("Failed to fetch price range", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error fetching price range:", error);
+    }
+  };
+
+  const handleDepositAmountChange = (amount, isToken1) => {
+    if (isToken1) {
+      setDepositAmount1(amount);
+      updatePriceRange(amount, selectedPair.token1, selectedPair.token2, true);
+    } else {
+      setDepositAmount2(amount);
+      updatePriceRange(amount, selectedPair.token2, selectedPair.token1, true);
+    }
+  };
+
   const handleAddLiquidity = async () => {
+    if (!isWalletConnected) {
+      console.error("Wallet is not connected");
+      return;
+    }
+
     const data = {
       lowerPrice: parseFloat(lowPrice),
       upperPrice: parseFloat(highPrice),
@@ -43,7 +122,6 @@ const AddLiquidity = () => {
       if (response.ok) {
         const result = await response.json();
         console.log("Liquidity added successfully:", result);
-        // Optionally, you can display a success message or reset the form fields here
       } else {
         console.error("Failed to add liquidity", response.statusText);
       }
@@ -55,8 +133,8 @@ const AddLiquidity = () => {
   return (
     <>
       <StarfieldAnimation
-        numParticles={500} // Customize the number of stars
-        depth={500} // Adjust star depth for a more immersive effect
+        numParticles={500}
+        depth={500}
         style={{
           position: "absolute",
           top: 0,
@@ -112,7 +190,7 @@ const AddLiquidity = () => {
           <div className="mb-4">
             <label className="block text-sm mb-1">Fee tier</label>
             <div className="flex justify-between space-x-2 fee-tier-card">
-              {[0.01, 0.05, 0.3, 1.0].map((tier) => (
+              {[0.2].map((tier) => (
                 <button
                   key={tier}
                   className={`flex-1 p-3 text-center rounded-md ${
@@ -129,27 +207,6 @@ const AddLiquidity = () => {
             </div>
           </div>
 
-          {/* Price Range */}
-          <div className="mb-4">
-            <label className="block text-sm mb-1">Set price range</label>
-            <div className="flex space-x-2 price-range">
-              <input
-                type="text"
-                value={lowPrice}
-                onChange={(e) => setLowPrice(e.target.value)}
-                placeholder="Low price"
-                className="flex-1 p-2 bg-gray-700 rounded-md focus:outline-none"
-              />
-              <input
-                type="text"
-                value={highPrice}
-                onChange={(e) => setHighPrice(e.target.value)}
-                placeholder="High price"
-                className="flex-1 p-2 bg-gray-700 rounded-md focus:outline-none"
-              />
-            </div>
-          </div>
-
           {/* Deposit Amounts */}
           <div className="mb-6">
             <label className="block text-sm mb-1">Deposit amounts</label>
@@ -157,27 +214,63 @@ const AddLiquidity = () => {
               <input
                 type="text"
                 value={depositAmount1}
-                onChange={(e) => setDepositAmount1(e.target.value)}
+                onChange={(e) =>
+                  handleDepositAmountChange(e.target.value, true)
+                }
                 placeholder={`Amount of ${selectedPair.token1}`}
                 className="flex-1 p-2 bg-gray-700 rounded-md focus:outline-none"
               />
               <input
                 type="text"
                 value={depositAmount2}
-                onChange={(e) => setDepositAmount2(e.target.value)}
+                onChange={(e) =>
+                  handleDepositAmountChange(e.target.value, false)
+                }
                 placeholder={`Amount of ${selectedPair.token2}`}
                 className="flex-1 p-2 bg-gray-700 rounded-md focus:outline-none"
               />
             </div>
           </div>
 
-          {/* Connect Wallet Button */}
-          <button
-            onClick={handleAddLiquidity}
-            className="w-full p-3 bg-purple-600 rounded-md text-white font-bold hover:bg-purple-700 transition duration-300"
-          >
-            Add Liquidity
-          </button>
+          {/* Price Range */}
+          <div className="mb-4">
+            <label className="block text-sm mb-1">Set price range</label>
+            <div className="flex space-x-2 price-range">
+              <input
+                type="text"
+                value={lowPrice}
+                readOnly
+                placeholder="Low price"
+                className="flex-1 p-2 bg-gray-700 rounded-md focus:outline-none"
+              />
+              <input
+                type="text"
+                value={highPrice}
+                readOnly
+                placeholder="High price"
+                className="flex-1 p-2 bg-gray-700 rounded-md focus:outline-none"
+              />
+            </div>
+          </div>
+
+          {/* TonConnectButton or Add Liquidity Button */}
+          {!isWalletConnected ? (
+            <div className="flex justify-center">
+              <button className="w-full p-3 rounded-md text-white font-bold transition duration-300 bg-purple-600 hover:bg-purple-700">Connect Wallet</button>
+            </div>
+          ) : (
+            <button
+              onClick={handleAddLiquidity}
+              className={`w-full p-3 rounded-md text-white font-bold transition duration-300 ${
+                isFormValid
+                  ? "bg-purple-600 hover:bg-purple-700"
+                  : "bg-gray-600 cursor-not-allowed"
+              }`}
+              disabled={!isFormValid}
+            >
+              Add Liquidity
+            </button>
+          )}
         </div>
       </div>
     </>

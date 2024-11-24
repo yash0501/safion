252f1153d399042ced2b63924a22741e0d605d04
Yash Garg <yashgarg@goodeducator.com>
2024-11-24 05:48:27 +0530

    Refactor code

--- contracts/wrappers/LiquidityPool.compile.ts
+++
@@ -1,9 +0,0 @@
-import { CompilerConfig } from '@ton/blueprint';
-
-export const compile: CompilerConfig = {
-    lang: 'tact',
-    target: 'contracts/liquidity_pool.tact',
-    options: {
-        debug: true,
-    },
-};
